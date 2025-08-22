8062ed9c91c32d792a2406096b0ba3d90a240a58
Sabal Shrestha <primeiscrime@Sabals-MacBook-Pro.local>
2025-08-22 10:14:37 +1000

    Deploy working WebHealthLambda with urllib and increased timeout

---
+++ SabalShrestha/sabal_shrestha/sabal_shrestha_stack.py
@@ -0,0 +1,37 @@
+from aws_cdk import (
+    Duration,
+    Stack,
+    aws_lambda as _lambda,
+    aws_iam
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class SabalShresthaStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "SabalShresthaQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+        lambda_function = _lambda.Function(
+            self, "SabalShresthaFunction",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            # handler="HelloWorld.lambda_handler",
+            handler="WebHealthLambda.lambda_handler",
+            code=_lambda.Code.from_asset("./modules"),
+            timeout=Duration.seconds(10)
+        )
+        # Add permission to publish metrics to CloudWatch
+        lambda_function.add_to_role_policy(
+            aws_iam.PolicyStatement(
+                effect=aws_iam.Effect.ALLOW,
+                actions=["cloudwatch:PutMetricData"],
+                resources=["*"]
+            )
+        )
