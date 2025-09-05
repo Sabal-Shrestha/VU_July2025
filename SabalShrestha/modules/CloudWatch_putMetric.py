ffd7b8d4d8aaf70504af9f0211b2442be5e03459
Sabal Shrestha <primeiscrime@Sabals-MacBook-Pro.local>
2025-09-05 13:14:40 +1000

    Fix cloudwatch_putmetric

--- SabalShrestha/modules/CloudWatch_putMetric.py
+++ SabalShrestha/modules/CloudWatch_putMetric.py
@@ -14,8 +14,6 @@ class CloudWatchMetricPublisher:
                     'MetricName': metric_name,
                     'Dimensions': dimensions,
                     'Value': value,
-                    # Enable high-resolution custom metrics (1-second granularity)
-                    'StorageResolution': 1,
                 }
             ]
         )
